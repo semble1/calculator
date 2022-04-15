cbb590c7d2202a27f9e2a39d226bdb7c3027b96f
semble1 <jaforkalam@gmail.com>
2022-04-15 15:37:32 -0400

    Added addition, subtraction, multiplication, and division functions.

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    num = a + b;
+    return num;
+}
+
+function subtract(a, b) {
+    num = a - b;
+    return num;
+}
+
+function multiply(a, b) {
+    num = a * b;
+    return num;
+}
+
+function divide(a, b) {
+    num = a / b;
+    return num;
+}
