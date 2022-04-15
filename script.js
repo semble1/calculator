b7383a473c0d3ffbd21933d3a3ec3b47f9da938d
semble1 <jaforkalam@gmail.com>
2022-04-15 15:44:25 -0400

    Added operate function.

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function divide(a, b) {
     num = a / b;
     return num;
 }
+
+function operate(a, b, operand) {
+    if (operand == '+') {
+        return add(a, b);
+    }
+    else if (operand == '-') {
+        return subtract(a, b);
+    }
+    else if (operand == '*') {
+        return multiply(a, b);
+    }
+    else if (operand == '/') {
+        return divide(a, b);
+    }
+    else {
+        return "OOPS";
+    }
+}
